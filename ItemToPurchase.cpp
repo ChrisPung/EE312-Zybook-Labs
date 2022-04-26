05df04389bf2a6c0e34f4f29cc248c25d5dbd30d
ChrisPung <77942171+ChrisPung@users.noreply.github.com>
2022-04-25 22:52:07 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -3,16 +3,18 @@ using namespace std;
 
 #include "ItemToPurchase.h"
 
-// Default Constructor.
+// Default Constructor. This will be called once for each object created to the ItemToPurchase class.
 ItemToPurchase::ItemToPurchase(){
    itemName = "none";
    itemPrice = 0;
    itemQuantity = 0;
 }
+// These functions are "Setters" or "Mutators". They change a class' data member.
 void     ItemToPurchase::SetName(string newName)      {itemName = newName;}
 void     ItemToPurchase::SetPrice(int newPrice)       {itemPrice = newPrice;}
 void     ItemToPurchase::SetQuantity(int newQuantity) {itemQuantity = newQuantity;}
 
+// These functions are "Getters" or "Accessors". They only access a class' data member. They do not change them.
 string   ItemToPurchase::GetName() const              {return itemName;}
 int      ItemToPurchase::GetPrice() const             {return itemPrice;}
 int      ItemToPurchase::GetQuantity() const          {return itemQuantity;}
